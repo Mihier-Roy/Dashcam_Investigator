1d561f9eff206dec52b7f4ee0bf4d282b2f4663b
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-06-08 11:42:44 +0100

    Configured logging for the application

--- dashcam_investigator/__main__.py
+++ dashcam_investigator/__main__.py
@@ -1,7 +1,23 @@
-"""
-This file is used to initialise the application and launch the Qt GUI.
-"""
+import logging
+import logging.config
+import os
 
 
 def main():
-    print("Running dashcam investigator")
+    logger = logging.getLogger(__name__)
+    logger.info("Running dashcam investigator info")
+
+
+if __name__ == "__main__":
+    # Create a logs directory in AppData\Local\DashcamInvestigator if it doesn't already exist
+    appdata_local = os.getenv("LOCALAPPDATA")
+    log_path = os.path.join(appdata_local, "DashcamInvestigator", "Logs")
+    log_path = log_path.replace("\\", "/")
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    # Setup logging based on log.conf
+    logging.config.fileConfig("log.conf", defaults={"logPath": log_path})
+
+    # Call main function
+    main()

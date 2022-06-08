98183455c59690220e03e8275a981cda9bf9d577
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-06-08 11:46:00 +0100

    Integrated the extraction functions so that the application can run

--- dashcam_investigator/__main__.py
+++ dashcam_investigator/__main__.py
@@ -1,11 +1,37 @@
 import logging
 import logging.config
+from pathlib import Path
+import tempfile
+from extract_metadata import (
+    extract_metadata_loop,
+    get_file_list,
+    make_handlers,
+)
 import os
 
 
 def main():
     logger = logging.getLogger(__name__)
     logger.info("Running dashcam investigator info")
+    logger.info(f"Temp dir set : {tempfile.gettempdir()}")
+
+    input_path = input("Enter a file path : ")
+    input_path = Path(input_path)
+    logger.debug(f"Obtained input path: {input_path}.")
+
+    logger.debug(f"Beginning analysis of input directory.")
+    video_list = get_file_list(input_directory=input_path)
+
+    logger.debug("Video files identified: ")
+    for video in video_list:
+        print(f"Name: {video}")
+
+    logger.debug(f"Beginning metadata extraction on {len(video_list)} files.")
+    meta_list = make_handlers(
+        video_name_list=video_list, temp_directory=tempfile.gettempdir()
+    )
+    extract_metadata_loop(meta_list, input_path)
+    logger.debug("Metadata extraction completed!")
 
 
 if __name__ == "__main__":

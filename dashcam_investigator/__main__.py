92453a1a7137712004c8d0fb2d7ef4ece87e8ddf
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-06-09 14:45:52 +0100

    Changed all path operations to use pathlib instead of the os lib

--- dashcam_investigator/__main__.py
+++ dashcam_investigator/__main__.py
@@ -1,19 +1,19 @@
 import logging
 import logging.config
 from pathlib import Path
+import os
 import tempfile
 from extract_metadata import (
     extract_metadata_loop,
     get_file_list,
     make_handlers,
 )
-import os
 
 
 def main():
     logger = logging.getLogger(__name__)
     logger.info("Running dashcam investigator info")
-    logger.info(f"Temp dir set : {tempfile.gettempdir()}")
+    logger.debug(f"Temp dir set : {tempfile.gettempdir()}")
 
     input_path = input("Enter a file path : ")
     input_path = Path(input_path)
@@ -37,10 +37,11 @@ def main():
 if __name__ == "__main__":
     # Create a logs directory in AppData\Local\DashcamInvestigator if it doesn't already exist
     appdata_local = os.getenv("LOCALAPPDATA")
-    log_path = os.path.join(appdata_local, "DashcamInvestigator", "Logs")
+    log_path = Path(appdata_local, "DashcamInvestigator", "Logs")
     log_path = log_path.replace("\\", "/")
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
+
+    if not Path(log_path).exists():
+        Path(log_path).mkdir(parents=True, exist_ok=True)
 
     # Setup logging based on log.conf
     logging.config.fileConfig("log.conf", defaults={"logPath": log_path})

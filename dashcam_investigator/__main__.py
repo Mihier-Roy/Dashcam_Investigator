48cc9f6a6444f3d1bf5308aa2087313164b54c23
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-06-16 11:03:42 +0100

    Specify argument to not disable existing loggers such that logging across modules works with the same config

--- dashcam_investigator/__main__.py
+++ dashcam_investigator/__main__.py
@@ -12,28 +12,26 @@ from extract_metadata import (
 from gui import app
 
 
-def main():
-    logger = logging.getLogger(__name__)
-    logger.info("Running dashcam investigator info")
-    logger.debug(f"Temp dir set : {tempfile.gettempdir()}")
+# def main():
+#     logger.debug(f"Temp dir set : {tempfile.gettempdir()}")
 
-    input_path = input("Enter a file path : ")
-    input_path = Path(input_path)
-    logger.debug(f"Obtained input path: {input_path}.")
+#     input_path = input("Enter a file path : ")
+#     input_path = Path(input_path)
+#     logger.debug(f"Obtained input path: {input_path}.")
 
-    logger.debug(f"Beginning analysis of input directory.")
-    video_list = get_file_list(input_directory=input_path)
+#     logger.debug(f"Beginning analysis of input directory.")
+#     video_list = get_file_list(input_directory=input_path)
 
-    logger.debug("Video files identified: ")
-    for video in video_list:
-        print(f"Name: {video}")
+#     logger.debug("Video files identified: ")
+#     for video in video_list:
+#         print(f"Name: {video}")
 
-    logger.debug(f"Beginning metadata extraction on {len(video_list)} files.")
-    meta_list = make_handlers(
-        video_name_list=video_list, temp_directory=tempfile.gettempdir()
-    )
-    extract_metadata_loop(meta_list, input_path)
-    logger.debug("Metadata extraction completed!")
+#     logger.debug(f"Beginning metadata extraction on {len(video_list)} files.")
+#     meta_list = make_handlers(
+#         video_name_list=video_list, temp_directory=tempfile.gettempdir()
+#     )
+#     extract_metadata_loop(meta_list, input_path)
+#     logger.debug("Metadata extraction completed!")
 
 
 if __name__ == "__main__":
@@ -41,13 +39,13 @@ if __name__ == "__main__":
     appdata_local = os.getenv("LOCALAPPDATA")
     log_path = Path(appdata_local, "DashcamInvestigator", "Logs")
     log_path = str(log_path).replace("\\", "/")
-
     if not Path(log_path).exists():
         Path(log_path).mkdir(parents=True, exist_ok=True)
 
     # Setup logging based on log.conf
-    logging.config.fileConfig("log.conf", defaults={"logPath": log_path})
+    logging.config.fileConfig(
+        "log.conf", defaults={"logPath": log_path}, disable_existing_loggers=False
+    )
 
-    # Call main function
-    # main()
+    # Launch GUI
     app.run()

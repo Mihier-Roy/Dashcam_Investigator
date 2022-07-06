b7165554750640939b9aa262dbf2eb14d1fe38de
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-06 14:26:23 +0100

    Updated extraction to output GPX files

--- dashcam_investigator/core/extract_metadata.py
+++ dashcam_investigator/core/extract_metadata.py
@@ -1,4 +1,3 @@
-from csv import reader, writer
 import logging
 from pathlib import Path
 from os import system
@@ -8,38 +7,13 @@ logger = logging.getLogger(__name__)
 
 def process_gps_data(video_path: Path, output_dir: Path) -> Path:
     """
-    Extracts GPS metadata from a video file and return the Path to the resulting CSV
+    Extracts GPS metadata from a video file and return the Path to the resulting GPX file
     """
     logger.debug(f"Extracting GPS data for -> {video_path.resolve()}")
-    output_csv = Path(output_dir, f"{video_path.name[0:-4]}_gpsdata.csv")
-    system(
-        f'exiftool  -ee -csv -p "$GPSSpeed, $GPSLatitude, $GPSLongitude" -r -n {video_path.resolve()} >> {output_csv.resolve()}'
-    )
-    trim_data(output_csv, output_dir)
-    logger.debug(f"GPS data available at -> {output_csv}")
-
-    return str(output_csv.resolve())
-
-
-def trim_data(output_csv: Path, output_dir: Path):
-    """
-    Removes rows in the exiftool csv output for gpsdata which can cause errors later in the code
-    """
-    temp_csv = Path(output_dir, f"{output_csv.name}.tmp")
-
-    with output_csv.open("r") as csvfile_read:
-        with temp_csv.open(
-            "w",
-            newline="",
-        ) as csvfile_write:
-            csv_writer = writer(csvfile_write)
-            for row in reader(csvfile_read):
-                if set(row).intersection(["SourceFile"]):
-                    continue
-                else:
-                    csv_writer.writerow(row)
-    output_csv.unlink()
-    temp_csv.rename(output_csv)
+    output_gpx = Path(output_dir, f"{video_path.name[0:-4]}.gpx")
+    system(f"exiftool -p gpx.fmt -ee3 {video_path.resolve()} > {output_gpx.resolve()}")
+    logger.debug(f"GPS data available at -> {output_gpx}")
+    return str(output_gpx.resolve())
 
 
 def process_file_meta(video_path: Path, output_dir: Path) -> Path:
@@ -52,19 +26,5 @@ def process_file_meta(video_path: Path, output_dir: Path) -> Path:
         f'exiftool -ee -FileType -filesize -MIMEType -d %d-%m-%Y" "%H:%M:%S -createDate -Duration -Format -Information -csv  {video_path.resolve()} >> {output_csv.resolve()}'
     )
     logger.debug(f"File metadata available at -> {output_csv}")
-
-    return str(output_csv.resolve())
-
-
-def process_time_meta(video_path: Path, output_dir: Path) -> Path:
-    """
-    Extracts Time metadata from a video file and return the Path to the resulting CSV
-    """
-    output_csv = Path(output_dir, f"{video_path.name[0:-4]}_timedata.csv")
-    logger.debug(f"Extracting time metadata for -> {video_path.resolve()}")
-    system(
-        f'exiftool -ee -d %d-%m-%Y" "%H:%M:%S -gpsdatetime -s -s -s {video_path.resolve()} >> {output_csv.resolve()}'
-    )
-    logger.debug(f"Time metadata available at -> {output_csv}")
 
     return str(output_csv.resolve())

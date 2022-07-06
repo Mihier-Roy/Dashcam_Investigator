0175f89298ad80f8d8771f3a35336475a6fe8edc
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-06 15:26:17 +0100

    Added classes and functions to create maps from dataframes

---
+++ dashcam_investigator/core/map_classes.py
@@ -0,0 +1,249 @@
+from folium import (
+    TileLayer,
+    Map,
+    Marker,
+    Icon,
+    LayerControl,
+    ColorLine,
+    Popup,
+    IFrame,
+    FeatureGroup,
+)
+from folium.vector_layers import PolyLine
+from folium.plugins import MeasureControl, Draw
+from branca import colormap
+from pandas import DataFrame
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class RouteLineMaker:
+
+    """
+    Takes in a dataframe containing coordinates and speeds from a single dashcam video.
+    Plots a single-coloured routeline on a folium map using those coordinates.
+    Plots a routeline which is coloured based on the speed of the vehicle.
+    Adds a marker to the start of the routeline with a popup containing information about the video file that is being plotted.
+    """
+
+    def __init__(
+        self,
+        gps_df: DataFrame,
+        points,
+        routeline_group: FeatureGroup,
+        start_marker_group: FeatureGroup,
+        colour_line_group: FeatureGroup,
+    ):
+
+        self.points = points
+        self.speed = gps_df["Speed"].to_list()
+        self.routeline_group = routeline_group
+        self.start_marker_group = start_marker_group
+        self.colour_line_group = colour_line_group
+
+    def make_routeline(self, routeline_colour: str):
+        """
+        Take in a routeline colour (either blue for extracted metadata or purple for watermark data),
+        and plot the coordinates stored by the class instance on the map
+        """
+        logger.debug("Generating route line using points from GPX file")
+        PolyLine(locations=self.points, color=routeline_colour).add_to(
+            self.routeline_group
+        )
+
+    def make_routeline_with_speed_colouring(self, colour_map=colormap.linear):
+        """
+        Take in a branca linear color map, and use this to plot an additional routeline which uses the speeds stored by the class instance
+        as a colour scale
+        """
+        logger.debug(
+            "Generating a routeline colored to indicate the speed of the vehicle"
+        )
+        ColorLine(
+            positions=self.points, colors=self.speed, colormap=colour_map, weight=4.5
+        ).add_to(self.colour_line_group)
+
+    def make_start_marker(self, popup: Popup):
+        """
+        Take in a popup for a start marker, which contains an HTML table of file details, and add it to a start marker for the routelines.
+        """
+        logger.debug("Generating a start marker")
+        Marker(
+            location=self.points[0],
+            tooltip=f"Start of route line. Click to see file details.",
+            popup=popup,
+            icon=Icon(icon="plus-circle", prefix="fa"),
+        ).add_to(self.start_marker_group)
+
+
+class StartMarkerPopup:
+    """
+    Parses the file information dataframe for a single video file into its separate components
+    Generates the popup used by the RouteLineMaker class for its start marker using the parsed file information.
+    """
+
+    def __init__(self, file_info_df):
+        self.file_name = file_info_df["SourceFile"].iloc[0]
+        self.file_type = file_info_df["FileType"].iloc[0]
+        self.file_size = file_info_df["FileSize"].iloc[0]
+        self.MIMEType = file_info_df["MIMEType"].iloc[0]
+        self.duration = file_info_df["Duration"].iloc[0]
+        self.average_speed = f'{file_info_df["AverageSpeed"].iloc[0]}'
+        self.max_speed = f'{file_info_df["MaxSpeed"].iloc[0]}'
+        self.file_create_date = file_info_df["CreateDate"].iloc[0]
+
+    def start_marker_popup_html(self) -> Popup:
+        logger.debug("Adding file info to start marker")
+        # Defines the HTML file details table
+        self.popup_html = (
+            """<html  lang="en">
+			<head>
+			<meta  charset="UTF-8">
+	<style> 
+	table {
+		font-family:  arial,  sans-serif; 
+		border-collapse:  collapse; 
+		width:  100%;
+	}
+
+	td,  th  {
+		border:  1px  solid  #dddddd; 
+		text-align:  left;  
+		padding:  8px;
+	}
+	 
+
+
+	tr:nth-child(even) { 
+		background-color:  #dddddd;
+	}
+	</style>
+	 
+			</head>
+			<body>
+			 
+
+			<h1>File Information</h1>
+			<table style ="width:100%">
+				<tr>	
+					<th> File  Attribute(s)</th>
+					<th> Value(s)</th>
+				</tr>	
+				<tr>	
+					<td>	Name:  </td>
+					<td> """
+            + self.file_name
+            + """ </td>
+				</tr>	
+				<tr>	
+					<td>	File Type  </td>
+					<td> """
+            + self.file_type
+            + """	 </td>
+				</tr>	
+				<tr>	
+					<td>	File Size  </td>
+					<td>	"""
+            + self.file_size
+            + """  </td>
+				</tr>	
+				<tr>	
+					<td>	MIME Type  </td>
+					<td>	"""
+            + self.MIMEType
+            + """   </td>
+				</tr>	
+				<tr>	
+					<td>	Video Length  </td>
+					<td>	"""
+            + self.duration
+            + """  </td>
+				</tr>
+				</tr>	
+				<tr>	
+					<td>	Average  Speed:	</td>
+					<td>	"""
+            + str(self.average_speed)
+            + """  </td>
+				</tr>	
+				<tr>	
+					<td>	Highest  Speed:	</td>
+					<td>	"""
+            + str(self.max_speed)
+            + """  </td>
+				</tr>
+				<tr>		 
+					<td>  File Create Date and Time:	</td>
+					<td>  """
+            + str(self.file_create_date)
+            + """  </td>
+				</tr>
+			</tr>
+		
+		</table>
+
+
+		</body>
+		</html>  
+		
+		"""
+        )
+        iframe = IFrame(html=self.popup_html, width=500, height=300)
+        popup = Popup(iframe, max_width=2650)
+
+        return popup
+
+
+class Mappy:
+    """Class which stores the folium map canvas, adds tilelayers, adds draw options, and adds featuregroups"""
+
+    def __init__(self, average_point):
+        self.canvas = Map(location=average_point, zoom_start=12)
+
+    def add_tilelayers(self):
+        # Adds different map styles which can bee freely switched between by the user
+        logger.debug("Adding layers to the folium map")
+        TileLayer("OpenStreet Map").add_to(self.canvas)
+        TileLayer("Stamen Terrain").add_to(self.canvas)
+        TileLayer("Stamen Toner").add_to(self.canvas)
+
+    def add_draw_options(self):
+        # Adds draw options for the user
+        logger.debug("Adding draw controls to the map")
+        Draw(
+            export=True,
+            filename="my_data.geojson",
+            position="topleft",
+            draw_options={"polyline": {"allowIntersection": False}},
+            edit_options={"poly": {"allowIntersection": False}},
+        ).add_to(self.canvas)
+
+    def generate_feature_groups(self):
+        """
+        Makes the required feature groups. These groups represent display layers, and will contain features displayed on the map.
+        Each feature group can be toggled on and off by the user from the interactive map file, with elements toggled on displayed on the map, and those toggled off removed.
+        """
+        logger.debug("Adding routelines and start markers to the map")
+        self.routelines = FeatureGroup(name="Route line", show=True).add_to(self.canvas)
+        self.speed_lines = FeatureGroup(
+            name="Route line coloured by speed", show=False
+        ).add_to(self.canvas)
+        self.start_markers = FeatureGroup(
+            name="Start markers for each video", show=True
+        ).add_to(self.canvas)
+        # self.speed_chart_marker = FeatureGroup(
+        #     name="Speed chart marker", show=True
+        # ).add_to(self.canvas)
+        # self.timeline_marker = FeatureGroup(name="Timeline marker", show=True).add_to(
+        #     self.canvas
+        # )
+        # self.exclusion_zone_feature_group=FeatureGroup(name="Exclusion zone group", show=True).add_to(self.canvas)
+
+    def add_layer_control(self):
+        # adds the ability to hide and show featuregroups/tilelayers
+        LayerControl().add_to(self.canvas)
+
+    def add_measure_control(self):
+        # adds the ability to measure the distance between two points
+        MeasureControl(position="bottomleft").add_to(self.canvas)
---
+++ dashcam_investigator/core/map_functions.py
@@ -0,0 +1,91 @@
+from branca.colormap import linear
+from folium import FeatureGroup
+from pandas import DataFrame
+from core.map_classes import Mappy, RouteLineMaker, StartMarkerPopup
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def initialise_map(video_metadata: DataFrame) -> Mappy:
+    # Initialises an instance of the Mappy class, which manages a folium map
+    logger.debug("Initialising map")
+    mappy = Mappy(video_metadata)
+    mappy.add_tilelayers()
+    mappy.add_draw_options()
+    mappy.generate_feature_groups()
+    mappy.add_layer_control()
+    mappy.add_measure_control()
+    return mappy
+
+
+def generate_speed_colour_map(speed: DataFrame) -> linear:
+    # Takes in a dataframe containing a large list of speed, and uses this to generate a branca linear colour map.
+    logger.debug("Generating color scale based on minimum and maximum speeds")
+    colour_map = linear.Set1_09.scale(speed["Speed"].min(), speed["Speed"].max())
+    colour_map.caption = "Speed colour scale: "
+    return colour_map
+
+
+def add_routeline_to_map(
+    gps_df: DataFrame,
+    points,
+    routeline_group: FeatureGroup,
+    start_marker_group: FeatureGroup,
+    colour_line_group: FeatureGroup,
+) -> RouteLineMaker:
+    """
+    Takes in a dataframe containing GPS and temporal data, as well as featuregroups for a routeline, start marker, and speed line.
+    Uses this data to generate an instance of the RouteLineMaker class, and returns the instance
+    """
+    logger.debug("Begin creating a routline")
+    routeliner = RouteLineMaker(
+        gps_df, points, routeline_group, start_marker_group, colour_line_group
+    )
+    return routeliner
+
+
+def add_start_marker_to_map(file_info_df: DataFrame, routeliner: RouteLineMaker):
+    """
+    Takes in a dataframe containing file information for a video and an instance of the RouteLineMaker class associated with that file.
+    Uses the file information dataframe to make a popup for a start marker. Calls the make_start_marker() method for the RouteLineMaker instance to add
+    the popup to a start marker
+    """
+    logger.debug("Begin creating a start marker")
+    marker_popup = StartMarkerPopup(file_info_df).start_marker_popup_html()
+    routeliner.make_start_marker(marker_popup)
+
+
+def add_speedline_to_map(routeliner: RouteLineMaker, colour_map: linear):
+    """
+    Takes in an instance of the RouteLineMaker class and a linear branca colormap.
+    Calls a method for the RouteLineMaker instance which uses the colour map to add a speed line to the map.
+    """
+    logger.debug("Begin creating a routline with speed coloring")
+    routeliner.make_routeline_with_speed_colouring(colour_map)
+
+
+def add_data_to_map(
+    video,
+    routelines: FeatureGroup,
+    start_markers: FeatureGroup,
+    speed_lines: FeatureGroup,
+    routeline_colour: str,
+    colour_map: linear,
+):
+    """
+    Takes in either a list of Video (video.py) or MetaDataFrames (dataframer.py) instances, three folium FeatureGroups, and a linear branca colour map.
+    Makes a RouteLineMaker (mapper_classes.py) instance for each video in the video list, and uses these instances to add routelines, start markers, and speed lines to a folium Map
+    """
+    logger.debug("Adding routlines, start markers to the map")
+    # for video in video_list:
+    routeliner = add_routeline_to_map(
+        gps_df=video.gps_df,
+        points=video.points,
+        routeline_group=routelines,
+        start_marker_group=start_markers,
+        colour_line_group=speed_lines,
+    )
+    routeliner.make_routeline(routeline_colour)
+    add_start_marker_to_map(video.file_info_df, routeliner)
+    add_speedline_to_map(routeliner, colour_map)
